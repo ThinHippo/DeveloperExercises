388c9e84f3bffe3b00e706802b4b6d2af880ff52
ThinHippo <nsoy@mail.ru>
2023-02-10 08:06:24 +0300

    Example002_HelloMasha

---
+++ Example002_HelloMasha/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа вывода уникального приветствия для конкретного имени
+Console.WriteLine("Познакомимся?");
+Console.WriteLine("Напишите своё имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == ("маша"))
+{
+  Console.WriteLine("Машка, привет! Давно не виделись!");
+}
+else
+{
+  Console.Write("Ну здравствуй, ");
+  Console.WriteLine(username.ToUpper());
+}
