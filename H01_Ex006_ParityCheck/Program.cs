9d05fd3d3236f23c49714fe0b1da99b3ea17ec42
ThinHippo <nsoy@mail.ru>
2023-02-11 18:32:57 +0300

    Программа проверки числа на четность

---
+++ H01_Ex006_ParityCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа на вход принимает число и выдаёт, является ли число чётным.
+Console.Clear();
+int num;
+Console.WriteLine("Введите целое число: ");
+num = int.Parse(Console.ReadLine());
+Console.Write(num);
+
+if (num % 2 == 0)
+{
+  Console.WriteLine(" -> да");
+}
+else
+{
+  Console.WriteLine(" -> нет");
+}
