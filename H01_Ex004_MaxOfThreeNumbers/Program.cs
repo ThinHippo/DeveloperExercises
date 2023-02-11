d9e426b122087b2563acd0a752ba486a0d4da2f0
ThinHippo <nsoy@mail.ru>
2023-02-11 17:59:20 +0300

    Программа получает три числа и выводит максимальное из них

---
+++ H01_Ex004_MaxOfThreeNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+int index = 0;
+int size = 3;
+double maxvalue = 0;
+double[] numbers = new double[size];
+
+Console.WriteLine("Введите последовательно три числа:");
+
+for (index = 0; index < numbers.Length; index++)
+{
+  numbers[index] = double.Parse(Console.ReadLine());
+
+  if (index == 0)
+  {
+    maxvalue = numbers[index];
+  }
+  if (numbers[index] > maxvalue)
+  {
+    maxvalue = numbers[index];
+  }
+
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(maxvalue);
