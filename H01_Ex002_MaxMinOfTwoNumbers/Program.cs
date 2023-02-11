5a2979806c62fdd8dd38d135fbb3256ec6814f76
ThinHippo <nsoy@mail.ru>
2023-02-11 16:50:31 +0300

    Максимальное и минимальное из двух чисел. Числа вводит пользователь.

---
+++ H01_Ex002_MaxMinOfTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число (F): ");
+double numF = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число (S): ");
+double numS = double.Parse(Console.ReadLine());
+
+if (numF > numS)
+{
+  Console.Write("Максимальное число: ");
+  Console.WriteLine(numF);
+  Console.Write("Минимальное число: ");
+  Console.WriteLine(numS);
+}
+else if (numF < numS)
+{
+  Console.Write("Максимальное число: ");
+  Console.WriteLine(numS);
+  Console.Write("Минимальное число: ");
+  Console.WriteLine(numF);
+}
+else
+{
+  Console.WriteLine("Числа равны");
+}
