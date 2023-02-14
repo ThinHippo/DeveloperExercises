b690c468eef8af3446b0fe535a5874521c45c25c
ThinHippo <nsoy@mail.ru>
2023-02-14 14:46:28 +0300

    Проверка двух чисел на кратность и вывод остатка

---
+++ S02_Ex003_Kratnost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+Console.Clear();
+Console.WriteLine("Введите последовательно два числа: ");
+int aNum = int.Parse(Console.ReadLine());
+int bNum = int.Parse(Console.ReadLine());
+if (bNum % aNum == 0)
+{
+  Console.WriteLine("да");
+}
+else {
+  Console.Write("Нет, остаток ");
+  Console.WriteLine(bNum % aNum);
+}
