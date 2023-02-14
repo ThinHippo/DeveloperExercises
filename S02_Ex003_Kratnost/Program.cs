68dd8df6da72f251115dc65b4f31ea7253cbd3d7
ThinHippo <nsoy@mail.ru>
2023-02-14 14:55:04 +0300

    С использованием void метода

--- S02_Ex003_Kratnost/Program.cs
+++ S02_Ex003_Kratnost/Program.cs
@@ -1,9 +1,8 @@
 ﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
 //является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 Console.Clear();
-Console.WriteLine("Введите последовательно два числа: ");
-int aNum = int.Parse(Console.ReadLine());
-int bNum = int.Parse(Console.ReadLine());
+
+void CheckKratnost (int a, int b) {
 if (bNum % aNum == 0)
 {
   Console.WriteLine("да");
@@ -12,3 +11,10 @@ else {
   Console.Write("Нет, остаток ");
   Console.WriteLine(bNum % aNum);
 }
+}
+
+Console.WriteLine("Введите последовательно два числа: ");
+int aNum = int.Parse(Console.ReadLine());
+int bNum = int.Parse(Console.ReadLine());
+
+CheckKratnost(aNum,bNum);
