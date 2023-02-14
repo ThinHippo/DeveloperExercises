6a741dfc07db45231acaedda638b2de540fb0f85
ThinHippo <nsoy@mail.ru>
2023-02-14 14:44:52 +0300

    Задача нахождения максимальной цифры в двухзначном числе

---
+++ S02_Ex001_MaxDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿//
+Console.Clear();
+int num = new Random().Next(10, 100);
+
+int fDigit = num / 10;
+int sDigit = num % 10;
+Console.WriteLine(num);
+if (fDigit > sDigit)
+{
+  Console.WriteLine(fDigit);
+}
+else if (fDigit < sDigit)
+{
+  Console.WriteLine(sDigit);
+}
+else
+{
+  Console.Write("Оба числа равны ");
+  Console.WriteLine(sDigit);
+}
