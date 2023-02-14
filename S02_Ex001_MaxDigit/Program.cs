b9c6de34e40a007d39ad113b715a8232d2f05305
ThinHippo <nsoy@mail.ru>
2023-02-14 15:00:38 +0300

    Решение с использованием метода

--- S02_Ex001_MaxDigit/Program.cs
+++ S02_Ex001_MaxDigit/Program.cs
@@ -1,20 +1,26 @@
 ﻿//
 Console.Clear();
-int num = new Random().Next(10, 100);
 
-int fDigit = num / 10;
-int sDigit = num % 10;
-Console.WriteLine(num);
-if (fDigit > sDigit)
+int MaxDigit()
 {
-  Console.WriteLine(fDigit);
+  int num = new Random().Next(10, 100);
+
+  int fDigit = num / 10;
+  int sDigit = num % 10;
+
+  Console.WriteLine(num);
+  if (fDigit > sDigit)
+  {
+    return fDigit;
+  }
+  else if (fDigit < sDigit)
+  {
+    return sDigit;
+  }
+  else
+  {
+    return sDigit;
+  }
 }
-else if (fDigit < sDigit)
-{
-  Console.WriteLine(sDigit);
-}
-else
-{
-  Console.Write("Оба числа равны ");
-  Console.WriteLine(sDigit);
-}
+
+Console.WriteLine (MaxDigit());
