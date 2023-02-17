46e974d1e72e894573e6dffb3171d084f390a373
ThinHippo <nsoy@mail.ru>
2023-02-17 12:35:30 +0300

    Решение завернуто в метод

--- S02_Ex003_Kratnost/Program.cs
+++ S02_Ex003_Kratnost/Program.cs
@@ -1,20 +1,20 @@
-﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+﻿int checkKratnost(int a, int b)
+{
+  return (b % a);
+}
+//Напишите программу, которая будет принимать на вход два числа и выводить, 
 //является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 Console.Clear();
-
-void CheckKratnost (int a, int b) {
-if (b % a == 0)
-{
-  Console.WriteLine("да");
-}
-else {
-  Console.Write("Нет, остаток ");
-  Console.WriteLine(b % a);
-}
-}
-
 Console.WriteLine("Введите последовательно два числа: ");
 int aNum = int.Parse(Console.ReadLine());
 int bNum = int.Parse(Console.ReadLine());
-
-CheckKratnost(aNum,bNum);
+int result;
+result = checkKratnost(aNum, bNum);
+if (result == 0)
+{
+  Console.WriteLine("Число " + bNum + " кратно " + aNum);
+}
+else
+{
+  Console.WriteLine("Число " + bNum + " не кратно " + aNum + " (остаток " + result + ")");
+}
