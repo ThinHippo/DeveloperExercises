15a1db2535518abb8e8ab09f2bd060307c339447
ThinHippo <nsoy@mail.ru>
2023-02-17 12:58:30 +0300

    Решение с использованием void метода

---
+++ S02_Ex003_MultipleOfOneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿//S02_Ex003_MultipleOfOneNumber
+void checkMultipleOfOneNumber(int a, int b)
+{
+  if (b % a == 0)
+  {
+    Console.WriteLine("Число " + b + " кратно " + a);
+  }
+  else
+  {
+    Console.WriteLine("Число " + b + " не кратно " + a + " (остаток " + b % a + ")");
+  }
+}
+//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+Console.Clear();
+Console.WriteLine("Введите последовательно два числа: ");
+int aNum = int.Parse(Console.ReadLine());
+int bNum = int.Parse(Console.ReadLine());
+
+checkMultipleOfOneNumber(aNum, bNum);
